2f115b7ca15393de2a5c1104d5886412d5564a55
necalculator <163542924+prosto-polzovatel-228@users.noreply.github.com>
2025-12-30 03:10:31 +0300

    Add files via upload

---
+++ all.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Список файлов с ботами
+bots = ["gr_storm.py", "gr_klin.py"]
+
+processes = []
+
+# Запускаем каждый бот отдельным процессом
+for bot in bots:
+    p = subprocess.Popen(["python3", bot])
+    processes.append(p)
+
+# Чтобы главный файл не завершился
+for p in processes:
+    p.wait()
